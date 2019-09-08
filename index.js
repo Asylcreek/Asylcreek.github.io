8131a87db2354efbb5cd651e2c8936d5175975b9
Asylcreek <omokugbobr@gmail.com>
2019-09-08 18:25:35 +0100

    Updated the .js and .php
    Added comments to both files.
    Added reload function to the .js
    Used condtional statements to the .php to validate the input data and hence prevent sending the form details.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 function validate(){
+   //Get input parameters from contact form
   var fNameField = document.getElementById('fname');
   var lNameField = document.getElementById('lname');
   var emailField = document.getElementById('email');
@@ -6,39 +7,49 @@ function validate(){
   var textField = document.getElementById('textarea');
   var emailFormat = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
+  //Conditional statements to validate the input parameters gotten
   if (fNameField.value ==="" && lNameField.value ==="" && emailField.value ==="" && titleField.value === "" && textField.value === "") {
    alert("Please fill all fields")
+   document.location.reload(true)
    }
   else if (fNameField.value === "") {
     alert("Please fill the First Name field");
-    return false
+    document.location.reload(true)
   }
   else if (lNameField.value === ""){
    alert("Please fill the Last Name field")
+   document.location.reload(true)
   }
   else if (emailField.value ==="") {
    alert("Please fill the Email field")
+   document.location.reload(true)
   }
   else if (titleField.value ==="") {
    alert("Please fill the Title field")
+   document.location.reload(true)
   }
   else if (textField.value ==="") {
    alert("Please fill the Message field")
+   document.location.reload(true)
   }
   else if (fNameField.value.length < 4){
    alert("Please your First Name should have at least 4 characters")
+   document.location.reload(true)
   }
   else if (lNameField.value.length < 4){
    alert("Please your Last Name should have at least 4 characters")
+   document.location.reload(true)
   }
   else if (textField.value.length < 20){
    alert("Please your Message should have at least 20 characters")
+   document.location.reload(true)
   }
   if (emailFormat.test(emailField.value)=== true){
       console.log("Valid Email Address")
    }
    else{
        alert("Invalid Email Address")
+       document.location.reload(true)
    }
-   
+
 }
--- index.php
+++ index.php
@@ -1,24 +1,40 @@
 <?php
 
 if(isset($_POST['Submit'])){
+	//Setting the email that receives the form data and the Subject of the email
 	$emailTo = "omokugbobr@gmail.com";
 	$emailSubject = "Data from CV Contact Form";
 
+	//Collecting input data from the Contact form
 	$fname = $_POST['FirstName'];
 	$lname = $_POST['LastName'];
 	$email = $_POST['EmailAddress'];
 	$title = $_POST['TitleofMessage'];
 	$message = $_POST['Message'];
-	
-	$emailMessage = "Contact Form details below:. \n\n";
+
+	//Create the email body
+	$emailMessage = "Contact Form details below: \n\n";
 	$emailMessage .= "First Name:  ".$fname."\n";
 	$emailMessage .= "Last Name:   ".$lname."\n";
 	$emailMessage .= "Email Address:   ".$email."\n";
 	$emailMessage .= "Title of Message:   ".$title."\n";
 	$emailMessage .= "Message:     ".$message."\n";
 
-	mail($emailTo, $emailSubject, $emailMessage);
-	echo "Mail sent";
-}
-
-?>
+	//Conditonal statements to prevent the sending on invalid input parameters
+	if (strlen($fname)==0 or strlen($fname) <4 or (!preg_match("/^[a-zA-Z ]*$/", $fname))) {
+		echo "Invalid input: Check if First Name is empty, has less than 4 characters, or has special characters";
+	}
+	elseif (strlen($lname)==0 or strlen($lname) < 4 or (!preg_match("/^[a-zA-Z ]*$/", $lname))) {
+		echo "Invalid input: Check if First Name is empty, has less than 4 characters, or has special characters";
+	}
+	elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+		echo "Invalid Email";
+	}
+	elseif (strlen($message)==0 or strlen($message)<20) {
+		echo "Invalid input";
+	}
+	else{
+		mail($emailTo, $emailSubject, $emailMessage);
+		echo "Mail sent";
+	}
+} 
